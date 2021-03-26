003915c811606695a2cc33bf40dddc55988bf74f
Eri <trgianghuynhdev1808@gmail.com>
2021-03-26 12:42:17 +0700

    imp:  data table UI

--- source/pages/index.vue
+++ source/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Hello world</div>
+  <div>
+    <h2>Demo exceljs</h2>
+    <v-btn depressed color="success">
+      Import
+      <v-icon right dark>
+        mdi-file-import
+      </v-icon>
+    </v-btn>
+    <v-btn class="ml-3" depressed color="primary">
+      Export
+      <v-icon right dark>
+        mdi-file-export
+      </v-icon>
+    </v-btn>
+    <SheetTable />
+  </div>
 </template>
 
 <script>
+import SheetTable from "~/components/SheetTable";
+
 export default {
-  components: {}
+  components: { SheetTable }
 };
 </script>
